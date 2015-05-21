0d0737dbaea561816403a3697101db0a7d0dba26
Matthew Witalinski <mateusz@applifactory.pl>
2015-05-21 09:21:31 +0200

    Fix content editable new lines

--- assets/js/app/directives/ContentEditableDirective.js
+++ assets/js/app/directives/ContentEditableDirective.js
@@ -35,6 +35,27 @@ app.directive("contenteditable", function($timeout, $window) {
         scope.$apply(read);
       });
 
+      element.bind('keydown', function(e) {
+        if( ( e.keyCode || e.witch ) == 13 ) {
+            e.preventDefault();
+            if( navigator.userAgent.indexOf("msie") > 0 ) {
+              insertHtml('<br />');
+            }
+            else {
+              var selection = window.getSelection(),
+              range = selection.getRangeAt(0),
+              br = document.createElement('br');
+              range.deleteContents();
+              range.insertNode(br);
+              range.setStartAfter(br);
+              range.setEndAfter(br);
+              range.collapse(false);
+              selection.removeAllRanges();
+              selection.addRange(range);
+            }
+        }
+      });
+
       element.bind('paste', function(e){
         e.preventDefault();
         var text = (e.originalEvent || e).clipboardData.getData('text/plain') || prompt('Paste something..');

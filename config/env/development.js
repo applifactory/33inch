4786ec553f8e3dd0db9dd93ccb6c11622aacdabe
Mateusz Witalinski <matthew@MacBook-Pro-Mateusz.local>
2015-07-08 20:23:44 +0200

    Fix mongo on new OSX

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://localhost/33inch_dev',
+  db: 'mongodb://localhost:27017/33inch_dev',
   port: 3000,
   secret: 't0q^)7#q*bh-s9k2qhxrxv+xw_eywwce%es0jlm3l)8mt2!36!',
   domain: '33inch.dev'

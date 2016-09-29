7a05a3d991c77ffe14ee65a9a445a3ffdf3c0f6d
Matthew Witalinski <mateusz@applifactory.pl>
2016-09-29 17:05:44 +0200

    Staging port

--- config/env/staging.js
+++ config/env/staging.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   db: 'mongodb://localhost/33inch_staging',
-  port: 80,
+  port: 3000,
   secret: '050W8OZjS-;t0D&7iuK0d5s[R%E~hb98t5fHA56eyPi38E0:6w|d94R70TX/3ia',
   domain: '33inch.com:3000',
   mailgun: {

5fed21b8241ec58ea78b6cae6464a957b61a8919
Matthew Witalinski <mateusz@applifactory.pl>
2016-09-29 17:04:29 +0200

    Add staging config

---
+++ config/env/staging.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  db: 'mongodb://localhost/33inch_staging',
+  port: 80,
+  secret: '050W8OZjS-;t0D&7iuK0d5s[R%E~hb98t5fHA56eyPi38E0:6w|d94R70TX/3ia',
+  domain: '33inch.com:3000',
+  mailgun: {
+    apiKey: 'key-0b1b17693f231d876ca352f0b04dbecb',
+    domain: 'sandboxf495f590ea7944b080912e4895e235db.mailgun.org'
+  }
+};

4c9c2401c33a59ef9094ac373d39160cdc81fa4a
Matthew Witalinski <mateusz@applifactory.pl>
2016-10-05 03:21:33 +0200

    Prevent mouse scrolling through sidebar

--- assets/js/app/directives/sidebar/SidebarDirective.js
+++ assets/js/app/directives/sidebar/SidebarDirective.js
@@ -2,6 +2,16 @@ app.directive('sidebar', function(){
   return {
     restrict: 'E',
     replace: true,
-    templateUrl: 'assets/app/website/sidebar/main.html'
+    templateUrl: 'assets/app/website/sidebar/main.html',
+    link: function(scope, el, attr) {
+      el.bind('mousewheel', function(e, d) {
+        if (
+             (el[0].scrollHeight - this.scrollTop - el[0].clientHeight <= 1 && e.deltaY > 0) ||
+             ( this.scrollTop <= 1 && e.deltaY < 0 )
+           ) {
+          e.preventDefault();
+        }
+      });
+    }
   }
 })

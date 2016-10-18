a434710bc8ea6ce69a5a6aaed4151b38973f0295
Mateusz Witalinski <mateusz.witalinski@gmail.com>
2016-10-18 21:51:51 +0200

    Cleanup websites list ctrl

--- assets/js/app/controllers/WebsitesListCtrl.js
+++ assets/js/app/controllers/WebsitesListCtrl.js
@@ -1,17 +1,3 @@
 app.controller('WebsitesListController', function($scope, websites){
-
-  console.log(websites);
   $scope.websites = websites;
-
-  console.log('WebsitesListController');
-  // $scope.$on('$viewContentLoaded', function(event){
-  //   if ( $state.current.name == 'app' ) {
-  //     $scope.isLoading = true;
-  //     WebsitesService.getAll().then(function(websites){
-  //       $scope.websites = websites;
-  //       $scope.isLoading = false;
-  //     });
-  //   }
-  // });
-
 });

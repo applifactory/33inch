4f2aa29c93535567c488a1c16a04e80f46432257
Matthew Witalinski <mateusz@applifactory.pl>
2016-11-17 09:36:24 +0100

    Fix slider

--- assets/js/inch33/slider.js
+++ assets/js/inch33/slider.js
@@ -1,38 +1,38 @@
 $(function(){
   $('.slides').each(function(){
-    $this = $(this);
-    $slides = $this.find('.slide');
+    var $this = $(this),
+        $slides = $this.find('.slide'),
+        $nav = $this.next();
     $slides.removeClass('active');
     $slides.first().addClass('active');
-    $nav = $this.next();
     _nav = '';
     $slides.each(function(){
       _nav += '<li></li>';
     });
     $nav.html(_nav);
-    $navs = $nav.find('li');
+    var $navs = $nav.find('li');
     var setActive = function(index) {
       $slides.removeClass('active');
       $navs.removeClass('active');
       $slides.eq(index).addClass('active');
       $navs.eq(index).addClass('active');
       timerStart();
-    }
+    };
     var next = function() {
       var index = $this.find('.active').index() + 1;
       if ( index >= $slides.length )
         index = 0;
       setActive(index);
-    }
+    };
     var _timeout = 0;
     var timerStart = function() {
       clearTimeout(_timeout);
       _timeout = setTimeout(next, 5000);
-    }
+    };
     $navs.bind('click', function(){
       setActive($(this).index());
-    })
+    });
     setActive(0);
     timerStart();
   });
-})
+});

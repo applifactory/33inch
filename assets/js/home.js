55ce4247fdf504bec49deae2511c89620b7962ac
Matthew Witalinski <mateusz@applifactory.pl>
2016-11-29 12:42:17 +0100

    Home navigation scripts

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,2 @@
 //= require vendors/jquery/dist/jquery.js
-//= require vendors/bootstrap/js/affix.js
 //= require_tree home
--- assets/js/home/scroll-down.js
+++ assets/js/home/scroll-down.js
@@ -1,6 +1,9 @@
 $(function(){
-  $('.scroll-down').click(function(e){
-    var $target = $('.newsletter');
-    $('html,body').stop().animate({scrollTop: Math.min($target.offset().top, $('body')[0].scrollHeight - $(window).height())}, 350);
-  })
-})
+  $('a[href^="#"]').click(function(e){
+    var $target = $(e.currentTarget.href.replace(/.+#/gi, '#'));
+    if ( $target.length ) {
+      e.preventDefault();
+      $('html,body').stop().animate({scrollTop: Math.min($target.offset().top, $('body')[0].scrollHeight - $(window).height())}, 350);
+    }
+  });
+});
